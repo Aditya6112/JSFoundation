83e9c6963c2623216158ebedacf05b7a8379c310
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2025-11-13 07:08:10 +0000

    Part1 covered

---
+++ part1/printing.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+console.log("Aditya");
+//printing in different lines
+process.stdout.write("Adiii:)");
+process.stdout.write("Adiii:)");
+//printing in same line thats the difference...
+
+//prinitng table
+console.table({name: "Aditya", age:22, company: "CTS"});
+//printing array
+console.warn({name:"Aditya", age:22});
