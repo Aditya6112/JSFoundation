1a00a7c64a1b60b29efe55dc1d849c5960e3f96f
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2025-11-13 07:24:12 +0000

    data types

---
+++ part1/datatypes.js
@@ -0,0 +1,25 @@
+/*
+String
+Number
+Boolean
+Bigint
+
+Undefined
+Null
+
+Object
+
+Symbol
+*/
+
+//var score = 102 (old school method)
+
+let score = 102
+let name = 'Aditya'
+
+//object
+let myTypes = ['Good', 'Skinny', 'Muscular'];
+let myUser = {firstname: "Aditya", lastname: "Raj"};
+
+let getScore = score;
+console.log(getScore);
