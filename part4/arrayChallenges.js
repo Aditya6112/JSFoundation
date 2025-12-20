e665f5f4da1279b1b449165542aade3f73eea058
ADITYA RAJ <raj8017933@gmail.com>
2025-12-21 00:53:54 +0530

    practice

---
+++ part4/arrayChallenges.js
@@ -0,0 +1,8 @@
+/*
+1. Declare an array named `teaFlavovors` that contains the strings:
+   `"green", "black", "herbal", "oolong", "white"`.
+   Access the first element of the array and store it in a variable named `firstTea`.
+*/
+let teaFlavovors = ["green", "black", "herbal", "oolong", "white"];
+let firstTea = teaFlavovors[0];
+console.log(firstTea); //Output: green
